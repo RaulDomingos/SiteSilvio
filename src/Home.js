d9e480fddebb034b44c973352c36e258ef7c52b4
SundaeRaul <raulrios0907@outlook.com>
2021-09-03 14:48:31 -0300

    itens trazidos da api  funcionando

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import './Home.css';
-import Workers from './img/workers.png';
-import Header from './Components/Header/index';
+import Header from './Components/Header/';
+import Sidenav from './Components/Sidenav/'
 
 function Home() {
   return (
     <div>
       <Header />
+      <Sidenav />
       <div className="container-fluid d-flex justify-content-center align-items-center">
         <div className="above-header position-absolute">
           <h3 className="col-10 m-3">Soluções em Laudos e Vistorias Técnicas para a sua segurança.</h3>
@@ -13,11 +14,10 @@ function Home() {
         </div>
       </div>
       <div className="section1 container-fluid">
-        <div className="container section1-content mt-5 d-flex align-items-center justify-content-center p-5">
-          <img className="workers img-fluid mt-5 pt-5 col-7" src={Workers} alt="Engenheiros trabalhando" />
-          <div className="section1-text position-absolute d-flex flex-column align-items-center justify-content-around col-4">
-            <h3 className="col-8 m-4 fs-2">Uma empresa especializada em trazer segurança ao seu negócio.</h3>
-            <p className="col-8 m-4 fs-5">A SD Engenharia possui expertise em laudos e vistorias para diversos seguimentos como: Playgrounds, Sistemas de Exaustão para Cozinhas Profissionais, Projetos de Ar condicionado e Planos de PMOC, NR-11, NR-12, Laudos para Máquinas Pesadas e Perícias Judiciais além de Responsabilidade técnica para empresas de Desmonte.</p>
+        <div className="container section1-content mt-5 p-5">
+          <div className="section-text position-relative mt-5 pt-5 col-8">
+            <h2 className="col-6">Uma empresa especializada em trazer segurança ao seu negócio.</h2>
+            <p className="col-10">A SD Engenharia possui expertise em laudos e vistorias para diversos seguimentos como: Playgrounds, Sistemas de Exaustão para Cozinhas Profissionais, Projetos de Ar condicionado e Planos de PMOC, NR-11, NR-12, Laudos para Máquinas Pesadas e Perícias Judiciais além de Responsabilidade técnica para empresas de Desmonte.</p>
           </div>
         </div>
       </div>
